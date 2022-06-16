c42e6977f0d1635fea7cf84f5823fb08d93d329f
Hieu Nguyen <nth937@gmail.com>
2022-06-16 16:56:31 +0300

    ex5.17 with db reset

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,22 +1,30 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'one',
+      username: 'user1',
+      password: 'passuser1'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
-  it('front page can be opened', function() {
+  xit('front page can be opened', function() {
     cy.contains('log in to application')
   })
 
+  it('login form is shown', function () {
+    cy.get('#username').should('be.visible')
+    cy.get('#password').should('be.visible')
+  })
+
   it('user can log in', function () {
-    // cy.get('input:first').type('user1')
-    // cy.get('input:last').type('passuser1')
     cy.get('#username').type('user1')
     cy.get('#password').type('passuser1')
-    // cy.contains('login').click()
     cy.get('#login-button').click()
 
     cy.contains('one logged in')
   })
-
-
 })
